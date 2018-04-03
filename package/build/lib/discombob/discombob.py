ec5750a9f122c89335d21eab2da39d7473150559
sbuckleybonanno <sbuckleybonanno@gmail.com>
2018-04-03 00:24:32 -0700

    Made the words split correctly. I'm going to have to use nltk next.

--- package/build/lib/discombob/discombob.py
+++ package/build/lib/discombob/discombob.py
@@ -6,11 +6,12 @@ class Essay(object):
 			self.text = f.read()
 		with open(api_key_filename) as f:
 			self.api_key = f.read()
-		this.split_text = re.findall("[a-zA-Z]+", self.text)
+		split_words = re.findall("[a-zA-Z]+", self.text)
 		seen = set()
 		seen_add = seen.add
-		self.words = [x for x in this.split_text if not (x in seen or seen_add(x))]
-	
+		self.words = [x for x in split_words if not (x in seen or seen_add(x))]
+		# print(self.words)
+		
 	# def discombobulate(self):
 		 
 		
