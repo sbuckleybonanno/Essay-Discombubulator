ac9f718fccfb19d9da2f5e0ffff791e58f1d2aaa
sbuckleybonanno <sbuckleybonanno@gmail.com>
2018-04-03 00:49:46 -0700

    Began to implement the request to the thesaurus api

--- package/discombob/discombob.py
+++ package/discombob/discombob.py
@@ -1,4 +1,6 @@
 import re
+import nltk
+import requests
 
 class Essay(object):
 	def __init__(self, text_filename, api_key_filename):
@@ -6,11 +8,16 @@ class Essay(object):
 			self.text = f.read()
 		with open(api_key_filename) as f:
 			self.api_key = f.read()
-		this.split_text = re.findall("[a-zA-Z]+", self.text)
+		self.split_text = re.findall("[a-zA-Z]+", self.text)
+		
+	
+	def discombobulate(self):
 		seen = set()
 		seen_add = seen.add
-		self.words = [x for x in this.split_text if not (x in seen or seen_add(x))]
-	
-	# def discombobulate(self):
-		 
-		
+		words = [x for x in self.split_text if not (x in seen or seen_add(x))] 
+		for word in words:
+			r = requests.get("http://words.bighugelabs.com/api/2/{0}/{1}/json".format(self.api_key, word))		
+			data = r.json()
+			print(data)
+			return
+
