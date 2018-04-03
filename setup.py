d3f9bb4422817ba8288f17ea8bd6cb213e9a831e
sbuckleybonanno <sbuckleybonanno@gmail.com>
2018-04-02 22:46:28 -0700

    Created basic Python library filestructure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Essay-Discombobulator',
+      version='1.0',
+      description='Changes every word to a synonym',
+      author='Samuel Buckley-Bonanno',
+      author_email='sbuckleybonanno@gmail.com',
+      url='https://github.com/sbuckleybonanno/Essay-Discombubulator',
+      packages=['discombob'],
+     )
+
